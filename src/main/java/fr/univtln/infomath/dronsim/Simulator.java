801d399cb57395dea2e713dd9ebe8c629cef6279
André Marçais <andre@marcais.net>
2025-05-13 18:20:44 +0200

    Add SLF4J logging dependency

--- src/main/java/fr/univtln/infomath/dronsim/Simulator.java
+++ src/main/java/fr/univtln/infomath/dronsim/Simulator.java
@@ -1,8 +1,13 @@
 package fr.univtln.infomath.dronsim;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Simulator {
+    private static final Logger log = LoggerFactory.getLogger(Simulator.class);
+
     public static void main(String[] args) {
-        System.err.println("Error: Refusing to run simulator. You are already in the matrix.");
+        log.error("Error: Refusing to run simulator. You are already in the matrix.");
         System.exit(1);
     }
 }
