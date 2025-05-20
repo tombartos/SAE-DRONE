88680eb497a6cbdf5d560496da2555e0c55a5aca
André Marçais <andre@marcais.net>
2025-05-20 15:12:13 +0200

    Add interface AUthenticationService
    All of the services will authenticate users useing this interface.

---
+++ src/main/java/fr/univtln/infomath/dronsim/auth/AuthenticationService.java
@@ -0,0 +1,20 @@
+package fr.univtln.infomath.dronsim.auth;
+
+/** Interface for interaction with authentication service.
+ */
+public interface AuthenticationService {
+    public AuthenticatedUser authenticate(String token) throws AuthenticationException;
+
+    @lombok.AllArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+    @lombok.Getter
+    public class AuthenticatedUser {
+        final String username;
+        final boolean isPilot;
+        final boolean isGameMaster;
+        final boolean isObserver;
+    }
+
+    //public static abstract class AuthorizationException extends Exception {}
+    public static abstract class AuthenticationException extends Exception {}
+    //public static abstract class NoSuchUser extends Exception {}
+}
